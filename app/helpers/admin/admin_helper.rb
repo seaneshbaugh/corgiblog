90750fd1ee4193e1ee0461bf19892196aa612036
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-04-28 17:35:17 -0500

    forgot to add get_gem_dependencies

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,2 +1,7 @@
 module Admin::AdminHelper
+  def get_gem_dependencies
+    lockfile = Bundler::LockfileParser.new(Bundler.read_file(Rails.root.join('Gemfile.lock')))
+
+    lockfile.specs.map { |spec| { :name => spec.name, :version => spec.version.version, :dependencies => spec.dependencies.map { |dependency| { :name => dependency.name, :requirement => dependency.requirement } } } }
+  end
 end
