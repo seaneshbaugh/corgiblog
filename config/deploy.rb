8b0cdeca7a6c23788e38f97f22c15dd54a988011
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-25 11:01:23 -0500

    Updated deploy config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,52 @@
-set :stages, %w(production)
-set :default_stage, 'production'
-
-require 'bundler/capistrano'
-require 'capistrano/ext/multistage'
-
-load 'config/recipes/base'
-load 'config/recipes/check'
-load 'config/recipes/db'
-load 'config/recipes/mysql'
-load 'config/recipes/smtp'
-load 'config/recipes/devise'
-load 'config/recipes/uploads'
-load 'config/recipes/tumblr'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
 set :application, 'corgiblog'
-set :user, 'conneyth'
-set :deploy_via, :remote_cache
-set :use_sudo, false
-set :scm, 'git'
-set :repository, 'git@github.com:seaneshbaugh/corgiblog.git'
-set :scm_verbose, true
+set :repo_url, 'git@github.com:seaneshbaugh/corgiblog.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/home/deployer/sites/conneythecorgi'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/application.yml', 'config/database.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
 set :bundle_flags, '--deployment'
 
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-
-after 'deploy', 'deploy:cleanup'
-
-load 'deploy/assets'
+set :rvm_ruby_version, '2.2.3'
 
 namespace :deploy do
-  namespace :assets do
-    desc 'Run the precompile task locally and rsync with shared'
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      %x{bundle exec rake assets:precompile}
-      %x{rsync --recursive --times --rsh=ssh --compress --human-readable --progress public/assets #{user}@#{domain}:#{shared_path}}
-      %x{bundle exec rake assets:clean}
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
     end
   end
+
 end
