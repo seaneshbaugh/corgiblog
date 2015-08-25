e75fe0a5aef5263b42368ef2b9cdcd7e994eb9cf
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-25 16:29:29 -0500

    Updated post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,21 @@ class PostsController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        @posts = Post.where(:visible => true).includes(:user).page(params[:page]).per(25).order('`posts`.`created_at` DESC')
+        if params[:tag].present?
+          @posts = Post.published.tagged_with(params[:tag]).includes(:user).page(params[:page]).per(25).reverse_chronological
+        else
+          @posts = Post.published.includes(:user).page(params[:page]).per(25).reverse_chronological
+        end
       end
 
       format.rss do
-        @posts = Post.where(:visible => true).includes(:user).order('`posts`.`created_at` DESC')
+        @posts = Post.published.includes(:user).reverse_chronological
       end
     end
   end
 
   def show
-    @post = Post.where(:slug => params[:id], :visible => true).first
+    @post = Post.published.where(slug: params[:id]).first
 
     if @post.nil?
       flash[:error] = t('messages.posts.could_not_find')
