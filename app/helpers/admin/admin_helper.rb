3bd38dfdd5b5480f13f0a262d62daada1880c543
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-26 10:53:45 -0500

    Updated admin helper.

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,11 +1,22 @@
 module Admin::AdminHelper
   def picture_inserter(target)
-    render :partial => 'shared/admin/picture_inserter', :locals => { :target => target }
+    render partial: 'shared/admin/picture_inserter', locals: { target: target }
   end
 
   def get_gem_dependencies
     lockfile = Bundler::LockfileParser.new(Bundler.read_file(Rails.root.join('Gemfile.lock')))
 
-    lockfile.specs.map { |spec| { :name => spec.name, :version => spec.version.version, :dependencies => spec.dependencies.map { |dependency| { :name => dependency.name, :requirement => dependency.requirement } } } }
+    lockfile.specs.map do |spec|
+      {
+       name: spec.name,
+       version: spec.version.version,
+       dependencies: spec.dependencies.map do |dependency|
+         {
+          name: dependency.name,
+          requirement: dependency.requirement
+         }
+       end
+      }
+    end
   end
 end
