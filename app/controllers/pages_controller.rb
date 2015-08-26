3dc6dd7b12564f9e555b30de49f3e899fa634f64
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-26 17:32:30 -0500

    Raise 404 instead of redirecting if a page is not found.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,7 @@ class PagesController < ApplicationController
     @page = Page.published.where(slug: params[:id]).first
 
     if @page.nil?
-      flash[:error] = t('messages.pages.could_not_find')
-
-      redirect_to root_url
+      raise ActionController::RoutingError.new('Not Found')
     end
   end
 end
