ef9c78750fbe056ad443de0f8f16c6ee4da2cb64
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-25 23:21:03 -0500

    Updated pages stuff.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def show
-    @page = Page.where(:slug => params[:id], :visible => true).first
+    @page = Page.published.where(slug: params[:id]).first
 
     if @page.nil?
       flash[:error] = t('messages.pages.could_not_find')
--- app/presenters/page_presenter.rb
+++ app/presenters/page_presenter.rb
@@ -3,24 +3,11 @@ class PagePresenter < BasePresenter
   include Linkable
 
   delegate :content_tag, to: :@template
-  delegate :contrast_color, to: :@template
 
   def initialize(page, template)
     super
 
     @page = page
-  end
-
-  def color_text
-    if @page.color.present?
-      "<span style=\"background: #{@page.color}; color: #{contrast_color(@page.color)}; padding: 3px;\">#{@page.color}</span>".html_safe
-    else
-      ''
-    end
-  end
-
-  def header_style
-    "color: #{@page.color}" if @page.color.present?
   end
 
   def show_in_menu
