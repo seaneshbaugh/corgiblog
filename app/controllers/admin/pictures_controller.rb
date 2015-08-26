e69ed960f3f94529286b5423633686788cf2220c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-26 17:36:34 -0500

    Updated admin pictures controller.

--- app/controllers/admin/pictures_controller.rb
+++ app/controllers/admin/pictures_controller.rb
@@ -1,102 +1,81 @@
 class Admin::PicturesController < Admin::AdminController
-  before_filter :authenticate_user!
-
   authorize_resource
+
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
   def index
     @search = Picture.search(params[:q])
 
-    @pictures = @search.result.page(params[:page]).per(25).order('`pictures`.`created_at` DESC')
+    @pictures = @search.result.page(params[:page]).per(25).reverse_chronological
   end
 
   def show
-    @picture = Picture.where(:id => params[:id]).first
-
-    if @picture.nil?
-      flash[:error] = t('messages.pictures.could_not_find')
-
-      redirect_to admin_pictures_url
-    end
   end
 
   def new
     @picture = Picture.new
   end
 
+  def edit
+  end
+
   def create
     respond_to do |format|
-      format.html {
-        @picture = Picture.new(params[:picture])
+      format.html do
+        @picture = Picture.new(picture_params)
 
         if @picture.save
-          flash[:success] = t('messages.pictures.created')
+          flash[:success] = 'Picture was successfully created.'
 
-          if params[:redirect_to_new].present?
-            redirect_to new_admin_picture_url
-          else
-            redirect_to admin_pictures_url
-          end
+          redirect_to admin_picture_url(@picture)
         else
-          flash[:error] = @picture.errors.full_messages.uniq.join('. ') + '.'
+          flash[:error] = view_context.error_messages_for(@picture)
 
           render 'new'
         end
-      }
-      format.js {
-        @picture = Picture.create(params[:picture])
-      }
-    end
-  end
+      end
 
-  def edit
-    @picture = Picture.where(:id => params[:id]).first
-
-    if @picture.nil?
-      flash[:error] = t('messages.pictures.could_not_find')
-
-      redirect_to admin_pictures_url
+      format.js do
+        @picture = Picture.create(picture_params)
+      end
     end
   end
 
   def update
-    @picture = Picture.where(:id => params[:id]).first
-
-    if @picture.nil?
-      flash[:error] = t('messages.pictures.could_not_find')
-
-      redirect_to admin_pictures_url and return
-    end
-
-    if @picture.update_attributes(params[:picture])
-      flash[:success] = t('messages.pictures.updated')
+    if @picture.update(picture_params)
+      flash[:success] = 'Picture was successfully updated.'
 
       redirect_to edit_admin_picture_url(@picture)
     else
-      flash[:error] = @picture.errors.full_messages.uniq.join('. ') + '.'
+      flash[:error] = view_context.error_messages_for(@picture)
 
       render 'edit'
     end
   end
 
   def destroy
-    @picture = Picture.where(:id => params[:id]).first
-
-    if @picture.nil?
-      flash[:error] = t('messages.pictures.could_not_find')
-
-      redirect_to admin_pictures_url and return
-    end
-
     @picture.destroy
 
-    flash[:success] = t('messages.pictures.deleted')
+    flash[:success] = 'Picture was successfully deleted.'
 
     redirect_to admin_pictures_url
   end
 
   def selector
-    @pictures = Picture.order('`pictures`.`created_at` DESC')
+    @pictures = Picture.reverse_chronological
 
-    render :layout => false
+    render layout: false
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.where(id: params[:id]).first
+
+    fail ActiveRecord::RecordNotFound if @picture.nil?
+  end
+
+  def picture_params
+    params.require(:picture).permit(:title, :alt_text, :caption, :image)
   end
 end
