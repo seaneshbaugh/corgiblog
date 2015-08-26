fb1848b0356eef7b1d7ab5be56f443726e78d97a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-26 15:32:32 -0500

    Updated seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,7 @@
-%x[rake db:data:load]
-
-load 'user.rb'
-
-users = User.all
-
-users.each do |user|
-  user.password = "changeme"
-  user.save!
-end
-
-sysadmin_user = User.create({ :email => 'sysadmin@conneythecorgi.com', :password =>'changeme', :role => 'sysadmin', :first_name => 'Sysadmin', :last_name => 'User' })
-
-sysadmin_user.save!
-
-admin_user = User.create({ :email => 'admin@conneythecorgi.com', :password =>'changeme', :role => 'admin', :first_name => 'Admin', :last_name => 'User' })
-
-admin_user.save!
-
-contributor_user = User.create({ :email => 'contributor@conneythecorgi.com', :password =>'changeme', :role => 'contributor', :first_name => 'Contributor', :last_name => 'User' })
-
-contributor_user.save!
-
-read_only_user = User.create({ :email => 'read_only@conneythecorgi.com', :password =>'changeme', :role => 'read_only', :first_name => 'Read Only', :last_name => 'User' })
-
-read_only_user.save!
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
