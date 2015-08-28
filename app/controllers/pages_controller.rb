4f565d71a92636e47d1ba449a9059c3e3270b923
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-08-27 22:48:36 -0500

    I guess this is a slightly better exception.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   def show
     @page = Page.published.where(slug: params[:id]).first
 
-    fail ActionController::RoutingError, 'Not Found' if @page.nil?
+    fail ActiveRecord::RecordNotFound if @page.nil?
   end
 end
