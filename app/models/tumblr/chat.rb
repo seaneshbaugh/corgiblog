49babdd319d135f22b95cb37eebfb1e82134cdcb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-09-01 21:31:42 -0500

    Added stuff for chats.

--- app/models/tumblr/chat.rb
+++ app/models/tumblr/chat.rb
@@ -1,4 +1,29 @@
 module Tumblr
   class Chat < PostFactory
+    def post_body
+      @body = content_tag(:ul, class: 'conversation') do
+        @json['dialogue'].each_with_index do |chat_item, index|
+          if (index + 1).even?
+            li_class = 'chat-even'
+          else
+            li_class = 'chat-odd'
+          end
+
+          concat(content_tag(:li, class: li_class) do
+            concat(content_tag(:span, chat_item['label'], class: 'label'))
+
+            concat("&quot;#{chat_item['phrase']}&quot;".html_safe)
+          end)
+        end
+      end
+    end
+
+    def post_title
+      @title = @json['title']
+
+      super unless @title.present?
+
+      @title
+    end
   end
 end
--- test/models/tumblr/chat_test.rb
+++ test/models/tumblr/chat_test.rb
@@ -2,5 +2,91 @@ require 'test_helper'
 
 module Tumblr
   class ChatTest < ActiveSupport::TestCase
+    test 'it should use the chat title as the post title if it exists' do
+      json = {
+        'id' => 1234567890,
+        'type' => 'chat',
+        'date' => '2015-08-24 21:59:07 GMT',
+        'tags' => [],
+        'title' => 'lolwut',
+        'body' => '',
+        'dialogue' => [
+          {
+            'label' => 'Casie:',
+            'name' => 'Casie',
+            'phrase' => 'lol'
+          },
+          {
+            'label' => 'Sean:',
+            'name' => 'Sean',
+            'phrase' => 'wut'
+          }
+        ]
+      }
+
+      tumblr_post = Tumblr::PostFactory.new(json)
+
+      post = tumblr_post.to_post
+
+      assert_equal 'lolwut', post.title
+    end
+
+    test 'it should create the body based on the dialogue' do
+      json = {
+        'id' => 1234567890,
+        'type' => 'chat',
+        'date' => '2015-08-24 21:59:07 GMT',
+        'tags' => [],
+        'title' => 'lolwut',
+        'body' => '',
+        'dialogue' => [
+          {
+            'label' => 'Casie:',
+            'name' => 'Casie',
+            'phrase' => 'time to kaizen 😞'
+          },
+          {
+            'label' => 'Sean:',
+            'name' => 'Sean',
+            'phrase' => '塊然'
+          },
+          {
+            'label' => 'Sean:',
+            'name' => 'Sean',
+            'phrase' => 'so i just got some of the chocolate peanut caramel protein bars'
+          },
+          {
+            'label' => 'Casie:',
+            'name' => 'Casie',
+            'phrase' => 'at least i figured it out before a drank another 3 days worth of decaffeinated coffee and had a headache'
+          }
+        ]
+      }
+
+      tumblr_post = Tumblr::PostFactory.new(json)
+
+      post = tumblr_post.to_post
+
+      expected_body = '<ul class="conversation">' +
+                      '<li class="chat-odd">' +
+                      '<span class="label">Casie:</span>' +
+                      '&quot;time to kaizen 😞&quot;' +
+                      '</li>' +
+                      '<li class="chat-even">' +
+                      '<span class="label">Sean:</span>' +
+                      '&quot;塊然&quot;' +
+                      '</li>' +
+                      '<li class="chat-odd">' +
+                      '<span class="label">Sean:</span>' +
+                      '&quot;so i just got some of the chocolate peanut caramel protein bars&quot;' +
+                      '</li>' +
+                      '<li class="chat-even">' +
+                      '<span class="label">Casie:</span>' +
+                      '&quot;at least i figured it out before a drank another 3 days worth of decaffeinated coffee and had a headache&quot;' +
+                      '</li>' +
+                      '</ul>'
+
+      assert_equal expected_body, post.body
+    end
   end
 end
