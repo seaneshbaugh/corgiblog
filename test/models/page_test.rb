68c9b18687df6a395ea15bef02f8a67c8e5d3c9c
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-09-09 16:07:44 -0500

    Added test for first_image.

--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
+  test 'it finds the first image in a page' do
+    page = pages(:page_with_images)
+
+    assert_equal 'http://conneythecorgi.com/image1.jpg', page.first_image
+  end
 end
