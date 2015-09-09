b927cb185465c1542ec7061716c42335cffcc91a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-09-09 14:37:11 -0500

    Updated quote tests.

--- test/models/tumblr/quote_test.rb
+++ test/models/tumblr/quote_test.rb
@@ -2,34 +2,37 @@ require 'test_helper'
 
 module Tumblr
   class QuoteTest < ActiveSupport::TestCase
-    test 'it should return a blockquote as the body' do
-      json = {
-        'id' => 1234567890,
-        'type' => 'quote',
-        'date' => '2015-08-24 21:59:07 GMT',
-        'tags' => [],
-        'text' => "You see, I can live with doubt, and uncertainty, and not knowing. I think it's much more interesting to live not knowing, than to have answers that might be wrong. I have approximate answers, and possible beliefs, and different degrees of certainty about different things, but I'm not absolutely sure about anything, and many things I don't know anything about, such as whether it means anything to ask, &quot;why are we here?&quot;... But I don't have to have an answer; I don't feel frightened by not knowing things.",
-        'source' => '<a href="https://en.wikipedia.org/wiki/Richard_Feynman" target="_blank">Richard Feynman</a> (via <a href="http://www.sgutranscripts.org/wiki/SGU_Episode_181#Quote_of_the_Week_.28.29" target="_blank">The Skeptics Guide to the Universe: Episode 181 Quote of the Week</a>)'
-      }
+    test 'it should use a truncated version of the text as the title' do
+      tumblr_post = Tumblr::PostFactory.new(tumblr_json(:quote, 90000000021))
 
-      post = Tumblr::PostFactory.new(json)
+      post = tumblr_post.to_post
 
-      assert_equal "<blockquote class=\"tumblr-quote\"><p class=\"tumblr-quote-text\">You see, I can live with doubt, and uncertainty, and not knowing. I think it's much more interesting to live not knowing, than to have answers that might be wrong. I have approximate answers, and possible beliefs, and different degrees of certainty about different things, but I'm not absolutely sure about anything, and many things I don't know anything about, such as whether it means anything to ask, &quot;why are we here?&quot;... But I don't have to have an answer; I don't feel frightened by not knowing things.</p><cite class=\"tumblr-quote-source\"> &mdash; <a href=\"https://en.wikipedia.org/wiki/Richard_Feynman\" target=\"_blank\">Richard Feynman</a> (via <a href=\"http://www.sgutranscripts.org/wiki/SGU_Episode_181#Quote_of_the_Week_.28.29\" target=\"_blank\">The Skeptics Guide to the Universe: Episode 181 Quote of the Week</a>)</cite></blockquote>", post.to_post.body
+      assert_equal 'Death is certain, replacing both the siren-song of Paradise and', post.title
     end
 
-    test 'it should use a truncated version of the text as the title' do
-      json = {
-        'id' => 1234567890,
-        'type' => 'quote',
-        'date' => '2015-08-24 21:59:07 GMT',
-        'tags' => [],
-        'text' => "Accepting death &mdash; by understanding that every life comes to an end, when time demands it. Loss of life is to be mourned, but only if the life was wasted.",
-        'source' => '<a href="https://en.wikipedia.org/wiki/Spock" target="_blank">Spock</a> (via <a href="http://www.sgutranscripts.org/wiki/SGU_Episode_504#Skeptical_Quote_of_the_Week_.281:25:30.29" target="_blank">The Skeptics Guide to the Universe: Episode 504 Quote of the Week</a>)'
-      }
+    test 'it should return a blockquote as the body' do
+      tumblr_post = Tumblr::PostFactory.new(tumblr_json(:quote, 90000000022))
 
-      post = Tumblr::PostFactory.new(json)
+      post = tumblr_post.to_post
 
-      assert_equal 'Accepting death &mdash; by understanding that every life comes to an', post.to_post.title
+      expected_body = '<blockquote class="tumblr-quote">' \
+                      '<p class="tumblr-quote-text">' \
+                      'Everything alive will die someday. ' \
+                      'But in the meantime I got to see her smile, and that made it OK for awhile. ' \
+                      'To look into her eyes was worth the eventual demise of earth.' \
+                      '</p>' \
+                      '<cite class="tumblr-quote-source"> ' \
+                      '&mdash; ' \
+                      '<p>' \
+                      '<a href="http://en.wikipedia.org/wiki/George_Hrab" target="_blank">George Hrab</a> ' \
+                      '(via <a href="http://www.sgutranscripts.org/wiki/SGU_Episode_282#Quote_of_the_Week_.28.29" ' \
+                      'class="tumblr_blog" target="_blank">' \
+                      'The Skeptics Guide to the Universe: Episode 282 Quote of the Week</a>)' \
+                      '</p>' \
+                      '</cite>' \
+                      '</blockquote>'
+
+      assert_equal expected_body, post.body
     end
   end
 end
