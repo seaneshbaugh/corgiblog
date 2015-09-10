1394e8138b09b34b4e82c59c5e882a97b84c0151
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-09-10 08:56:16 -0500

    oops.

--- 20150909162033_add_sticky_to_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddStickyToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :sticky, :boolean, after: :visible, null: false, default: false
-
-    add_index :posts, :sticky
-  end
-end
