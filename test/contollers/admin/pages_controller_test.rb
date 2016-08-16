34e654b360acc25ac4923f775388080eed4bfb0f
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-16 14:41:26 -0500

    Fixed admin pages controller tests.

--- test/contollers/admin/pages_controller_test.rb
+++ test/contollers/admin/pages_controller_test.rb
@@ -10,36 +10,34 @@ module Admin
       get admin_pages_url
 
       assert_response :success
-
-#      assert_not_nil assigns(:pages)
     end
 
     test 'should get new' do
-      get :new
+      get new_admin_page_url
 
       assert_response :success
     end
 
     test 'should create page' do
       assert_difference('Page.count') do
-        post :create, params: { page: { title: 'New Page' } }
+        post admin_pages_url, params: { page: { title: 'New Page' } }
       end
 
-      assert_redirected_to admin_page_path(assigns(:page))
+      page = Page.last
+
+      assert_redirected_to admin_page_path(page)
     end
 
     test 'should not create invalid page' do
       assert_no_difference('Page.count') do
-        post :create, page: { title: '' }
+        post admin_pages_url, params: { page: { title: '' } }
       end
-
-      assert_template :new
     end
 
     test 'should show page' do
       page = pages(:about)
 
-      get :show, id: page
+      get admin_page_url(id: page)
 
       assert_response :success
     end
@@ -47,7 +45,7 @@ module Admin
     test 'should get edit' do
       page = pages(:about)
 
-      get :edit, id: page
+      get edit_admin_page_url(id: page)
 
       assert_response :success
     end
@@ -55,24 +53,24 @@ module Admin
     test 'should update page' do
       page = pages(:about)
 
-      patch :update, params: { id: page, page: { name: 'Updated Page' } }
+      patch admin_page_url(id: page), params: { page: { name: 'Updated Page' } }
 
-      assert_redirected_to edit_admin_page_path(assigns(:page))
+      page.reload
+
+      assert_redirected_to edit_admin_page_path(page)
     end
 
     test 'should not update page with invalid data' do
       page = pages(:about)
 
-      patch :update, id: page, page: { title: '' }
-
-      assert_template :edit
+      patch admin_page_url(id: page), params: { page: { title: '' } }
     end
 
     test 'should destroy page' do
       page = pages(:about)
 
       assert_difference('Page.count', -1) do
-        delete :destroy, id: page
+        delete admin_page_url(id: page)
       end
 
       assert_redirected_to admin_pages_path
