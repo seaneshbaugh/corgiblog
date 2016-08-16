251129ebc37e47c0a99f46a642a28f7311385581
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-16 16:12:01 -0500

    Updated admin versions tests.

--- test/controllers/admin/versions_controller_test.rb
+++ test/controllers/admin/versions_controller_test.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
 
 module Admin
-  class VersionsControllerTest < ActionController::TestCase
+  class VersionsControllerTest < ActionDispatch::IntegrationTest
     setup do
       sign_in users(:sean_eshbaugh)
     end
 
     test 'should get index' do
-      get :index
+      get admin_versions_url
 
       assert_response :success
-
-      assert_not_nil assigns(:versions)
     end
   end
 end
