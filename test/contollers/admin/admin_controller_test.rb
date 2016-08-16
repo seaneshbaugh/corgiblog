472b1b7086e69df547c7f7dbabc7f1b6feba94df
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-16 12:47:46 -0500

    Added tests for admin controller.

--- test/contollers/admin/admin_controller_test.rb
+++ test/contollers/admin/admin_controller_test.rb
@@ -1,6 +1,15 @@
 require 'test_helper'
 
 module Admin
-  class AdminControllerTest < ActionController::TestCase
+  class AdminControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      sign_in users(:sean_eshbaugh)
+    end
+
+    test 'should get the admin root' do
+      get admin_root_url
+
+      assert_response :success
+    end
   end
 end
