a6bd28eb69ba37ade4b978e71a7c5c75bcbd1592
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-16 12:44:35 -0500

    Set paper trail whodunnit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_action :set_paper_trail_whodunnit
+
   before_action -> { @pages = Page.published.in_menu.by_order }
 
   def after_sign_out_path_for(_resource_or_scope)
