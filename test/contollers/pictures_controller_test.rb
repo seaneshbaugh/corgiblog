a7a5ba578d1f3fb58afb3d117166c147910d2e4a
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-16 11:11:32 -0500

    Added pictures controller tests.

--- test/contollers/pictures_controller_test.rb
+++ test/contollers/pictures_controller_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
-class PicturesControllerTest < ActionController::TestCase
+class PicturesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get pictures_url
+
+    assert_response :success
+  end
+
+  test 'should get picture' do
+    image = fixture_file_upload(File.join('images', 'friday.jpg'), 'image/jpeg')
+
+    picture = Picture.create(title: 'Friday', image: image)
+
+    get picture_url(id: picture)
+
+    assert_response :success
+  end
 end
