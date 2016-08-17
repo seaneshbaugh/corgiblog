e4265256e3b02dd40525d3dca58b35ed38408712
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-17 15:42:41 -0500

    Nested calls should have parenthesis.

--- app/models/tumblr/quote.rb
+++ app/models/tumblr/quote.rb
@@ -2,7 +2,7 @@ module Tumblr
   class Quote < PostFactory
     def post_body
       @body = content_tag('blockquote', class: 'tumblr-quote') do
-        concat(content_tag :p, @json['text'].html_safe, class: 'tumblr-quote-text')
+        concat(content_tag(:p, @json['text'].html_safe, class: 'tumblr-quote-text'))
 
         concat(content_tag('cite', class: 'tumblr-quote-source') do
           concat(' &mdash; '.html_safe)
