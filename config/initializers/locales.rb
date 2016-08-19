b3fe7261012362af1bb6afdd8a91fcd74d551273
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-19 10:38:14 -0500

    Removed unecessary to_s in glob.

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,1 +1,1 @@
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
