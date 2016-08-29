02bf6335f89436aa960cb11a6a8afac718a734f4
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-29 14:32:11 -0500

    Cleaned up and I18nized PagePresenter.

--- app/presenters/page_presenter.rb
+++ app/presenters/page_presenter.rb
@@ -2,38 +2,19 @@ class PagePresenter < BasePresenter
   include FrontEnd
   include Linkable
 
-  delegate :content_tag, to: :@template
-
   def self.display_method
     :title
-  end
-
-  def self.humanized_attribute_names
-    @humanized_attribute_names ||= HashWithIndifferentAccess.new({
-      id: 'ID',
-      title: 'Title',
-      slug: 'Slug',
-      body: 'Body',
-      style: 'Style',
-      meta_description: 'Meta Description',
-      meta_keywords: 'Meta Keywords',
-      order: 'Order',
-      show_in_menu: 'Show in Menu?',
-      visible: 'Visible?',
-      created_at: 'Created At',
-      updated_at: 'Updated At'
-    })
   end
 
   def self.version_display_attributes
     [
       {
         method: :title,
-        header_class: 'col-xs-5',
+        header_class: 'col-xs-5'
       },
       {
         method: :order,
-        header_class: 'col-xs-1',
+        header_class: 'col-xs-1'
       },
       {
         method: :visible,
@@ -50,17 +31,17 @@ class PagePresenter < BasePresenter
 
   def show_in_menu
     if @page.show_in_menu
-      'Yes'
+      t('yes')
     else
-      'No'
+      t('no')
     end
   end
 
   def visible
     if @page.visible
-      'Yes'
+      t('yes')
     else
-      'No'
+      t('no')
     end
   end
 
