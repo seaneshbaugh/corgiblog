7c4efe985a7e8d9ea9080dad96bbb1263c6a0e75
Sean Eshbaugh <seaneshbaugh@gmail.com>
2016-08-29 14:29:22 -0500

    Delegation moved to BasePresenter.

--- app/presenters/concerns/front_end.rb
+++ app/presenters/concerns/front_end.rb
@@ -1,10 +1,6 @@
 module FrontEnd
   extend ActiveSupport::Concern
 
-  included do
-    delegate :content_tag, to: :@template
-  end
-
   def body_tag
     content_tag(:div, @object.body.html_safe, class: 'body-content')
   end
